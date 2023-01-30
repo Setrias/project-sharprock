5d6e0556b21e721d16e32c9b9be30ee330142a68
Setrias <tomas.buchtik@borsice.net>
2023-01-30 09:30:45 +0100

    písemka 2023-01-30

---
+++ pole_v4_2023-01-30/pole_v4_2023-01-30/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace pole_v4_2023_01_30
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPocetCisel = new System.Windows.Forms.TextBox();
+            this.buttonPocetCisel = new System.Windows.Forms.Button();
+            this.buttonZadatCisla = new System.Windows.Forms.Button();
+            this.textBoxCisla = new System.Windows.Forms.TextBox();
+            this.listBoxCisla = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pole v4";
+            // 
+            // textBoxPocetCisel
+            // 
+            this.textBoxPocetCisel.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxPocetCisel.Location = new System.Drawing.Point(16, 42);
+            this.textBoxPocetCisel.Name = "textBoxPocetCisel";
+            this.textBoxPocetCisel.Size = new System.Drawing.Size(100, 23);
+            this.textBoxPocetCisel.TabIndex = 1;
+            // 
+            // buttonPocetCisel
+            // 
+            this.buttonPocetCisel.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonPocetCisel.Location = new System.Drawing.Point(122, 42);
+            this.buttonPocetCisel.Name = "buttonPocetCisel";
+            this.buttonPocetCisel.Size = new System.Drawing.Size(107, 23);
+            this.buttonPocetCisel.TabIndex = 2;
+            this.buttonPocetCisel.Text = "Zadat počet čísel";
+            this.buttonPocetCisel.UseVisualStyleBackColor = true;
+            this.buttonPocetCisel.Click += new System.EventHandler(this.buttonPocetCisel_Click);
+            // 
+            // buttonZadatCisla
+            // 
+            this.buttonZadatCisla.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonZadatCisla.Location = new System.Drawing.Point(122, 86);
+            this.buttonZadatCisla.Name = "buttonZadatCisla";
+            this.buttonZadatCisla.Size = new System.Drawing.Size(107, 23);
+            this.buttonZadatCisla.TabIndex = 4;
+            this.buttonZadatCisla.Text = "Zadat číslo";
+            this.buttonZadatCisla.UseVisualStyleBackColor = true;
+            this.buttonZadatCisla.Click += new System.EventHandler(this.buttonZadatCisla_Click);
+            // 
+            // textBoxCisla
+            // 
+            this.textBoxCisla.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxCisla.Location = new System.Drawing.Point(16, 86);
+            this.textBoxCisla.Name = "textBoxCisla";
+            this.textBoxCisla.Size = new System.Drawing.Size(100, 23);
+            this.textBoxCisla.TabIndex = 3;
+            // 
+            // listBoxCisla
+            // 
+            this.listBoxCisla.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listBoxCisla.FormattingEnabled = true;
+            this.listBoxCisla.ItemHeight = 16;
+            this.listBoxCisla.Location = new System.Drawing.Point(235, 14);
+            this.listBoxCisla.Name = "listBoxCisla";
+            this.listBoxCisla.Size = new System.Drawing.Size(120, 148);
+            this.listBoxCisla.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 186);
+            this.Controls.Add(this.listBoxCisla);
+            this.Controls.Add(this.buttonZadatCisla);
+            this.Controls.Add(this.textBoxCisla);
+            this.Controls.Add(this.buttonPocetCisel);
+            this.Controls.Add(this.textBoxPocetCisel);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPocetCisel;
+        private System.Windows.Forms.Button buttonPocetCisel;
+        private System.Windows.Forms.Button buttonZadatCisla;
+        private System.Windows.Forms.TextBox textBoxCisla;
+        private System.Windows.Forms.ListBox listBoxCisla;
+    }
+}
+
---
+++ pole_v4_2023-01-30/pole_v4_2023-01-30/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pole_v4_2023_01_30
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBoxCisla.Enabled = false;
+            buttonZadatCisla.Enabled = false;
+        }
+
+        private int pocetCisel, zadCislo, pocetZadanych;
+        private int[] poleCisel;
+
+        private void buttonPocetCisel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                pocetCisel = Convert.ToInt32(textBoxPocetCisel.Text);
+
+                poleCisel = new int[pocetCisel];    // vytvoření pole o požadované velikosti
+
+                textBoxPocetCisel.Enabled = false;
+                buttonPocetCisel.Enabled = false;
+
+                textBoxCisla.Enabled = true;
+                buttonZadatCisla.Enabled = true;
+            }
+            catch
+            {
+                MessageBox.Show("Zadej číslo");
+            }
+        }
+
+        private void buttonZadatCisla_Click(object sender, EventArgs e)
+        {
+            zadCislo = Convert.ToInt32(textBoxCisla.Text);
+
+            poleCisel[pocetZadanych] = zadCislo;    // ukládání do pole
+            pocetZadanych++;
+
+            if (pocetZadanych == pocetCisel)
+            {
+                textBoxCisla.Enabled = false;
+                buttonZadatCisla.Enabled = false;
+
+                for (int i = 0; i < poleCisel.Length; i++)      // výpis do listBoxu
+                {
+                    listBoxCisla.Items.Add(poleCisel[i]);
+                }
+            }
+        }
+
+    }
+}
