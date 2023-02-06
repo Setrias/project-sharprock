83130f24e337c82a15dedd6b9a16a06f7bcd336b
Setrias <tomas.buchtik@borsice.net>
2023-02-06 10:42:07 +0100

    kreslení kružnice

---
+++ kresleni_kruznice/kresleni_kruznice/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace kresleni_kruznice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelKresPlocha = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panelKresPlocha
+            // 
+            this.panelKresPlocha.Location = new System.Drawing.Point(12, 78);
+            this.panelKresPlocha.Name = "panelKresPlocha";
+            this.panelKresPlocha.Size = new System.Drawing.Size(500, 500);
+            this.panelKresPlocha.TabIndex = 0;
+            this.panelKresPlocha.Paint += new System.Windows.Forms.PaintEventHandler(this.panelKresPlocha_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kreslení kružnice";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 590);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelKresPlocha);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelKresPlocha;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ kresleni_kruznice/kresleni_kruznice/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kresleni_kruznice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panelKresPlocha_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics kresPlocha = e.Graphics;
+
+            kresPlocha.DrawLine(Pens.Black, 0,0,500,0);         // top horizontal
+            kresPlocha.DrawLine(Pens.Red, 0,250,500,250);       // mid horizontal
+            kresPlocha.DrawLine(Pens.Blue, 0,499,500,499);      // bottom horizontal
+
+            kresPlocha.DrawLine(Pens.Blue, 0,0,0,500);          // left vertical
+            kresPlocha.DrawLine(Pens.Green, 250,0,250,500);     // bottom vertical
+            kresPlocha.DrawLine(Pens.BlueViolet, 499,0,499,500);    // right vertical
+
+            kresPlocha.DrawEllipse(Pens.Red, 100,100,300,300);
+        }
+    }
+}
