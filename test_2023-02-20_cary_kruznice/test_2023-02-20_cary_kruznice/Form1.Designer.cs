92b15d885341155a35bdcb52404c76637112dba4
Setrias <tomas.buchtik@borsice.net>
2023-02-20 09:35:54 +0100

    program z testu 2023-02-20

---
+++ test_2023-02-20_cary_kruznice/test_2023-02-20_cary_kruznice/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿
+namespace test_2023_02_20_cary_kruznice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelTest = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panelTest
+            // 
+            this.panelTest.Location = new System.Drawing.Point(12, 12);
+            this.panelTest.Name = "panelTest";
+            this.panelTest.Size = new System.Drawing.Size(300, 200);
+            this.panelTest.TabIndex = 0;
+            this.panelTest.Paint += new System.Windows.Forms.PaintEventHandler(this.panelTest_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 219);
+            this.Controls.Add(this.panelTest);
+            this.Name = "Form1";
+            this.Text = "Test";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelTest;
+    }
+}
+
---
+++ test_2023-02-20_cary_kruznice/test_2023-02-20_cary_kruznice/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_2023_02_20_cary_kruznice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panelTest_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics kresPanel = e.Graphics;
+
+            kresPanel.DrawLine(Pens.Crimson, 0, 0, 300, 200);
+            kresPanel.DrawLine(Pens.Crimson, 150, 0, 150, 200);
+
+            kresPanel.DrawEllipse(Pens.Crimson, 0, 160, 40, 40);
+        }
+    }
+}
