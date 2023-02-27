2cdd3642ddbc4dbd168960f5941f227b5e11e8d4
Setrias <tomas.buchtik@borsice.net>
2023-02-27 10:40:54 +0100

    kreslení combobox

---
+++ kresleni_combobox/kresleni_combobox/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kresleni_combobox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Graphics kresPlocha;
+        private int hodnotaX, sourX, hodnotaY, sourY, polomer;
+
+        private void panelKresleni_Paint(object sender, PaintEventArgs e)
+        {
+            kresPlocha = panelKresleni.CreateGraphics();
+        }
+
+        private void comboBoxTvary_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Soustředné kružnice
+            // Čtverce
+            // Náhodné kružnice
+            // Vlastní tvorba
+
+            if (comboBoxTvary.SelectedIndex == 0)
+            {
+                int velikost = 75; // základní velikost
+                int operVelikost = velikost; // operativní velikost kružnice, univerzální k různým velikostem 
+                sourX = 300 - velikost / 2;
+                sourY = 300 - velikost / 2;
+
+                for (int i = 0; i < 5; i++)
+                {
+                    kresPlocha.DrawEllipse(Pens.Crimson, sourX, sourY, velikost, velikost);
+                    sourX -= operVelikost / 2;
+                    sourY -= operVelikost / 2;
+                    velikost += operVelikost;
+                }
+            }
+
+            if (comboBoxTvary.SelectedIndex == 1)
+            {
+                int velikost = 50;
+                int operVelikost = velikost;
+                int pocetCtvercu = 5;
+                sourX = 300 - pocetCtvercu * (velikost / 2);
+                sourY = 300 - pocetCtvercu * (velikost / 2);
+
+                for (int i = 0; i < pocetCtvercu; i++)
+                {
+                    kresPlocha.DrawRectangle(Pens.Crimson, sourX, sourY, velikost, velikost);
+                    velikost += operVelikost;
+                }
+            }
+
+            if (comboBoxTvary.SelectedIndex == 2)
+            {
+            }
+
+            if (comboBoxTvary.SelectedIndex == 3)
+            {
+            }
+        }
+    }
+}
