cffbeb89ac4032bb6c2394d3c784ba55d239fa3d
Setrias <tomas.buchtik@borsice.net>
2023-02-27 10:40:54 +0100

    kreslení combobox

---
+++ kresleni_combobox/kresleni_combobox/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace kresleni_combobox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxTvary = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelKresleni = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxTvary
+            // 
+            this.comboBoxTvary.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxTvary.FormattingEnabled = true;
+            this.comboBoxTvary.Items.AddRange(new object[] {
+            "Soustředné kružnice",
+            "Čtverce",
+            "Náhodné kružnice",
+            "Vlastní tvorba"});
+            this.comboBoxTvary.Location = new System.Drawing.Point(74, 45);
+            this.comboBoxTvary.Name = "comboBoxTvary";
+            this.comboBoxTvary.Size = new System.Drawing.Size(148, 24);
+            this.comboBoxTvary.TabIndex = 0;
+            this.comboBoxTvary.SelectedIndexChanged += new System.EventHandler(this.comboBoxTvary_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kreslení / Combobox";
+            // 
+            // panelKresleni
+            // 
+            this.panelKresleni.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelKresleni.Location = new System.Drawing.Point(228, 45);
+            this.panelKresleni.Name = "panelKresleni";
+            this.panelKresleni.Size = new System.Drawing.Size(600, 600);
+            this.panelKresleni.TabIndex = 2;
+            this.panelKresleni.Paint += new System.Windows.Forms.PaintEventHandler(this.panelKresleni_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Zvol tvar:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 663);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panelKresleni);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxTvary);
+            this.Name = "Form1";
+            this.Text = "Kreslení / Combobox";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxTvary;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelKresleni;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ kresleni_combobox/kresleni_combobox/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kresleni_combobox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Graphics kresPlocha;
+        // private int hodnotaX, sourX, hodnotaY, sourY, polomer;
+
+        private void panelKresleni_Paint(object sender, PaintEventArgs e)
+        {
+            kresPlocha = panelKresleni.CreateGraphics();
+        }
+
+        private void comboBoxTvary_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Soustředné kružnice
+            // Čtverce
+            // Náhodné kružnice
+            // Vlastní tvorba
+
+            if (comboBoxTvary.SelectedIndex == 0)
+            {
+                panelKresleni.Refresh();
+                
+                int velikost = 75; // základní velikost
+                int pevnaVelikost = velikost; // operativní velikost kružnice, univerzální k různým velikostem 
+                int sourX = 300 - velikost / 2; // výpočet rohu kružnice
+                int sourY = 300 - velikost / 2;
+
+                Pen pero = new Pen(Color.Crimson, width: 3);
+
+                for (int i = 0; i < 5; i++)
+                {
+                    kresPlocha.DrawEllipse(pero, sourX, sourY, velikost, velikost);
+                    sourX -= pevnaVelikost / 2; // souřadnice X - pevná velikost / 2
+                    sourY -= pevnaVelikost / 2;
+                    velikost += pevnaVelikost;
+                }
+            }
+
+            if (comboBoxTvary.SelectedIndex == 1)
+            {
+                panelKresleni.Refresh();
+                
+                int velikost = 50;
+                int operVelikost = velikost;
+                int pocetCtvercu = 5;
+                int sourX = 300 - pocetCtvercu * (velikost / 2);
+                int sourY = 300 - pocetCtvercu * (velikost / 2);
+
+                Pen pero = new Pen(Color.DeepSkyBlue, width: 3);
+
+                for (int i = 0; i < pocetCtvercu; i++)
+                {
+                    kresPlocha.DrawRectangle(pero, sourX, sourY, velikost, velikost);
+                    velikost += operVelikost;
+                }
+            }
+
+            if (comboBoxTvary.SelectedIndex == 2)
+            {
+                panelKresleni.Refresh();
+
+                Random randNum = new Random(); // souřadnice X
+
+                for (int i = 0; i < 25; i++)
+                {
+                    int sourX = randNum.Next(-50, 500);
+                    int sourY = randNum.Next(-50, 500);
+                    int prumer = randNum.Next(20, 300);
+
+                    if (sourX == sourY)
+                    {
+                        sourY = randNum.Next(0, 500);
+                    }
+                    
+                    if (prumer == sourX || prumer == sourY)
+                    {
+                        prumer = randNum.Next(20, 300);
+                    }
+
+                    Pen pero = new Pen(Color.Black, width: 1);
+
+                    kresPlocha.DrawEllipse(pero, sourX, sourY, prumer, prumer);
+                }
+            }
+
+            if (comboBoxTvary.SelectedIndex == 3)
+            {
+                
+            }
+        }
+    }
+}
