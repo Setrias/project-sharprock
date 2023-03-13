3040c29eb1c08bf8f306f5ea9c84ade75e16dd3e
Setrias <tomas.buchtik@borsice.net>
2023-03-13 10:41:10 +0100

    tíhové zrychlení, úkol

---
+++ tihove_zrychleni_2023-03-13/tihove_zrychleni_2023-03-13/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace tihove_zrychleni_2023_03_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxVyska = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxCas = new System.Windows.Forms.TextBox();
+            this.buttonZadat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(307, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tíhové zrychlení s využitím volného pádu";
+            // 
+            // textBoxVyska
+            // 
+            this.textBoxVyska.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxVyska.Location = new System.Drawing.Point(60, 48);
+            this.textBoxVyska.Name = "textBoxVyska";
+            this.textBoxVyska.Size = new System.Drawing.Size(100, 23);
+            this.textBoxVyska.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Výška:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Čas:";
+            // 
+            // textBoxCas
+            // 
+            this.textBoxCas.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxCas.Location = new System.Drawing.Point(60, 77);
+            this.textBoxCas.Name = "textBoxCas";
+            this.textBoxCas.Size = new System.Drawing.Size(100, 23);
+            this.textBoxCas.TabIndex = 3;
+            // 
+            // buttonZadat
+            // 
+            this.buttonZadat.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonZadat.Location = new System.Drawing.Point(166, 48);
+            this.buttonZadat.Name = "buttonZadat";
+            this.buttonZadat.Size = new System.Drawing.Size(75, 52);
+            this.buttonZadat.TabIndex = 5;
+            this.buttonZadat.Text = "Zadat hodnoty";
+            this.buttonZadat.UseVisualStyleBackColor = true;
+            this.buttonZadat.Click += new System.EventHandler(this.buttonZadat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonZadat);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxCas);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxVyska);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Tíhové zrychlení";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxVyska;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxCas;
+        private System.Windows.Forms.Button buttonZadat;
+    }
+}
+
---
+++ tihove_zrychleni_2023-03-13/tihove_zrychleni_2023-03-13/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tihove_zrychleni_2023_03_13
+{
+    public partial class Form1 : Form
+    {
+        // Vypočítejte tíhové zrychlení s využitím volného pádu, provedeme n měření, kuličku
+        // spouštíme z výšky l, měříme čas t, pro každé měření vypočítat tíhové zrychlení,
+        // výsledné zrychlení je pak průměrem těchto hodnot
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        struct tihoveZrychleni
+        {
+            public double cas;
+            public double vyska;
+            public double tihZrych;
+        }
+
+        private tihoveZrychleni[] mereni = new tihoveZrychleni[20];
+
+        private void buttonZadat_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
