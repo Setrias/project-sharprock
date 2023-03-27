f2f13a7232ce1035b7beeee5158489fd09b8f5c6
Setrias <tomas.buchtik@borsice.net>
2023-03-27 10:40:28 +0200

    hrací pole, aby se vykreslily až po zadání všech

--- hraci_pole/hraci_pole/Form1.cs
+++ hraci_pole/hraci_pole/Form1.cs
@@ -26,9 +26,10 @@ namespace hraci_pole
             public string barva;
         }
 
-        private zetonyPar[] zeton = new zetonyPar[6];
+        private zetonyPar[] zetony = new zetonyPar[6];
 
         private int rozmer = 10, pocetZetonu, sourX, sourY;
+        private bool konec;
 
         // private Pen pero = new Pen(Color.Crimson, 3);
         private Brush point = new SolidBrush(Color.Black);
@@ -37,30 +38,31 @@ namespace hraci_pole
         {
             if (comboBoxBarva.Text != "")
             {
-                zeton[pocetZetonu].poziceX = Convert.ToInt32(numericUpDownSourX.Value);
-                zeton[pocetZetonu].poziceY = Convert.ToInt32(numericUpDownSourY.Value);
-                zeton[pocetZetonu].barva = comboBoxBarva.Text;
+                zetony[pocetZetonu].poziceX = Convert.ToInt32(numericUpDownSourX.Value);
+                zetony[pocetZetonu].poziceY = Convert.ToInt32(numericUpDownSourY.Value);
+                zetony[pocetZetonu].barva = comboBoxBarva.Text;
                 
-                switch (zeton[pocetZetonu].barva)
-                {
-                    case "Černá":
-                        point = new SolidBrush(Color.Black);
-                        break;
+                // switch (zetony[pocetZetonu].barva)
+                // {
+                //     case "Černá":
+                //         point = new SolidBrush(Color.Black);
+                //         break;
+                //
+                //     case "Bílá":
+                //         point = new SolidBrush(Color.White);
+                //         break;
+                // }
 
-                    case "Bílá":
-                        point = new SolidBrush(Color.White);
-                        break;
-                }
+                // sourX = zetony[pocetZetonu].poziceX * 50 - rozmer / 2;
+                // sourY = zetony[pocetZetonu].poziceY * 50 - rozmer / 2;
 
-                sourX = zeton[pocetZetonu].poziceX * 50 - rozmer / 2;
-                sourY = zeton[pocetZetonu].poziceY * 50 - rozmer / 2;
-
-                hraciPole.FillEllipse(point, sourX, sourY, rozmer, rozmer);
+                // hraciPole.FillEllipse(point, sourX, sourY, rozmer, rozmer);
 
                 pocetZetonu++;
 
                 if (pocetZetonu == 6)
                 {
+                    konec = true;
                     buttonZadat.Enabled = false;
                     numericUpDownSourX.Enabled = false;
                     numericUpDownSourY.Enabled = false;
@@ -77,16 +79,44 @@ namespace hraci_pole
             listBoxZetony.Items.Clear();
             for (int i = 0; i < pocetZetonu; i++)
             {
-                if (zeton[i].barva == "Černá")
+                if (zetony[i].barva == "Černá")
                 {
-                    listBoxZetony.Items.Add("Souřadnice X: " + zeton[i].poziceX + ", Souřadnice Y:" + zeton[i].poziceY);
+                    listBoxZetony.Items.Add("Souřadnice X: " + zetony[i].poziceX + ", Souřadnice Y:" + zetony[i].poziceY);
                 }
             }
         }
 
+        private int j;
         private void panelHraciPole_Paint(object sender, PaintEventArgs e)
         {
             hraciPole = panelHraciPole.CreateGraphics();
+
+            if (konec)
+            {
+                for (int i = 0; i < pocetZetonu; i++)
+                {
+                    sourX = zetony[j].poziceX * 50 - rozmer / 2;
+                    sourY = zetony[j].poziceY * 50 - rozmer / 2;
+                    hraciPole.FillEllipse(barvaBodu(), sourX, sourY, rozmer, rozmer);
+                    j++;
+                }
+            }
+        }
+
+        private Brush barvaBodu()
+        {
+            switch (zetony[j].barva)
+            {
+                case "Černá":
+                    point = new SolidBrush(Color.Black);
+                    break;
+
+                case "Bílá":
+                    point = new SolidBrush(Color.White);
+                    break;
+            }
+
+            return point;
         }
     }
 }
