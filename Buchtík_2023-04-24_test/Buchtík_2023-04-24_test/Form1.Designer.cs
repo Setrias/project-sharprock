65f0494e1b5027cb841125d03088030868819002
Setrias <tomas.buchtik@borsice.net>
2023-04-24 09:33:30 +0200

    Test 2023-04-24

---
+++ Buchtík_2023-04-24_test/Buchtík_2023-04-24_test/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Buchtík_2023_04_24_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonUlozit = new System.Windows.Forms.Button();
+            this.textBoxUkol1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonPocetSlov = new System.Windows.Forms.Button();
+            this.textBoxPocetSlov = new System.Windows.Forms.TextBox();
+            this.textBoxUkol2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxUkol4 = new System.Windows.Forms.TextBox();
+            this.buttonPrvniSlovo = new System.Windows.Forms.Button();
+            this.textBoxPrvniSlovo = new System.Windows.Forms.TextBox();
+            this.textBoxUkol3 = new System.Windows.Forms.TextBox();
+            this.textBoxDruhVety = new System.Windows.Forms.TextBox();
+            this.buttonDruhVety = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonUlozit
+            // 
+            this.buttonUlozit.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUlozit.Location = new System.Drawing.Point(12, 60);
+            this.buttonUlozit.Name = "buttonUlozit";
+            this.buttonUlozit.Size = new System.Drawing.Size(268, 23);
+            this.buttonUlozit.TabIndex = 0;
+            this.buttonUlozit.Text = "Uložit";
+            this.buttonUlozit.UseVisualStyleBackColor = true;
+            this.buttonUlozit.Click += new System.EventHandler(this.buttonUlozit_Click);
+            // 
+            // textBoxUkol1
+            // 
+            this.textBoxUkol1.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUkol1.Location = new System.Drawing.Point(12, 31);
+            this.textBoxUkol1.Name = "textBoxUkol1";
+            this.textBoxUkol1.Size = new System.Drawing.Size(268, 23);
+            this.textBoxUkol1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Úkol 1.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Úkol 2.";
+            // 
+            // buttonPocetSlov
+            // 
+            this.buttonPocetSlov.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPocetSlov.Location = new System.Drawing.Point(12, 178);
+            this.buttonPocetSlov.Name = "buttonPocetSlov";
+            this.buttonPocetSlov.Size = new System.Drawing.Size(268, 23);
+            this.buttonPocetSlov.TabIndex = 4;
+            this.buttonPocetSlov.Text = "Počet slov";
+            this.buttonPocetSlov.UseVisualStyleBackColor = true;
+            this.buttonPocetSlov.Click += new System.EventHandler(this.buttonPocetSlov_Click);
+            // 
+            // textBoxPocetSlov
+            // 
+            this.textBoxPocetSlov.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPocetSlov.Location = new System.Drawing.Point(12, 207);
+            this.textBoxPocetSlov.Name = "textBoxPocetSlov";
+            this.textBoxPocetSlov.ReadOnly = true;
+            this.textBoxPocetSlov.Size = new System.Drawing.Size(268, 23);
+            this.textBoxPocetSlov.TabIndex = 5;
+            // 
+            // textBoxUkol2
+            // 
+            this.textBoxUkol2.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUkol2.Location = new System.Drawing.Point(12, 149);
+            this.textBoxUkol2.Name = "textBoxUkol2";
+            this.textBoxUkol2.Size = new System.Drawing.Size(268, 23);
+            this.textBoxUkol2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(302, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 19);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Úkol 4.";
+            // 
+            // textBoxUkol4
+            // 
+            this.textBoxUkol4.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUkol4.Location = new System.Drawing.Point(302, 149);
+            this.textBoxUkol4.Name = "textBoxUkol4";
+            this.textBoxUkol4.Size = new System.Drawing.Size(275, 23);
+            this.textBoxUkol4.TabIndex = 8;
+            // 
+            // buttonPrvniSlovo
+            // 
+            this.buttonPrvniSlovo.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPrvniSlovo.Location = new System.Drawing.Point(302, 178);
+            this.buttonPrvniSlovo.Name = "buttonPrvniSlovo";
+            this.buttonPrvniSlovo.Size = new System.Drawing.Size(275, 23);
+            this.buttonPrvniSlovo.TabIndex = 9;
+            this.buttonPrvniSlovo.Text = "První slovo";
+            this.buttonPrvniSlovo.UseVisualStyleBackColor = true;
+            this.buttonPrvniSlovo.Click += new System.EventHandler(this.buttonPrvniSlovo_Click);
+            // 
+            // textBoxPrvniSlovo
+            // 
+            this.textBoxPrvniSlovo.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPrvniSlovo.Location = new System.Drawing.Point(302, 207);
+            this.textBoxPrvniSlovo.Name = "textBoxPrvniSlovo";
+            this.textBoxPrvniSlovo.ReadOnly = true;
+            this.textBoxPrvniSlovo.Size = new System.Drawing.Size(275, 23);
+            this.textBoxPrvniSlovo.TabIndex = 10;
+            // 
+            // textBoxUkol3
+            // 
+            this.textBoxUkol3.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUkol3.Location = new System.Drawing.Point(303, 31);
+            this.textBoxUkol3.Name = "textBoxUkol3";
+            this.textBoxUkol3.Size = new System.Drawing.Size(275, 23);
+            this.textBoxUkol3.TabIndex = 14;
+            // 
+            // textBoxDruhVety
+            // 
+            this.textBoxDruhVety.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDruhVety.Location = new System.Drawing.Point(303, 89);
+            this.textBoxDruhVety.Name = "textBoxDruhVety";
+            this.textBoxDruhVety.ReadOnly = true;
+            this.textBoxDruhVety.Size = new System.Drawing.Size(275, 23);
+            this.textBoxDruhVety.TabIndex = 13;
+            // 
+            // buttonDruhVety
+            // 
+            this.buttonDruhVety.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDruhVety.Location = new System.Drawing.Point(303, 60);
+            this.buttonDruhVety.Name = "buttonDruhVety";
+            this.buttonDruhVety.Size = new System.Drawing.Size(275, 23);
+            this.buttonDruhVety.TabIndex = 12;
+            this.buttonDruhVety.Text = "Druh věty";
+            this.buttonDruhVety.UseVisualStyleBackColor = true;
+            this.buttonDruhVety.Click += new System.EventHandler(this.buttonDruhVety_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(303, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 19);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Úkol 3.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 253);
+            this.Controls.Add(this.textBoxUkol3);
+            this.Controls.Add(this.textBoxDruhVety);
+            this.Controls.Add(this.buttonDruhVety);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxPrvniSlovo);
+            this.Controls.Add(this.buttonPrvniSlovo);
+            this.Controls.Add(this.textBoxUkol4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxUkol2);
+            this.Controls.Add(this.textBoxPocetSlov);
+            this.Controls.Add(this.buttonPocetSlov);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxUkol1);
+            this.Controls.Add(this.buttonUlozit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox textBoxUkol3;
+        private System.Windows.Forms.TextBox textBoxDruhVety;
+        private System.Windows.Forms.Button buttonDruhVety;
+        private System.Windows.Forms.Label label4;
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxUkol4;
+        private System.Windows.Forms.Button buttonPrvniSlovo;
+        private System.Windows.Forms.TextBox textBoxPrvniSlovo;
+
+        private System.Windows.Forms.TextBox textBoxUkol2;
+
+        private System.Windows.Forms.Button buttonPocetSlov;
+        private System.Windows.Forms.TextBox textBoxPocetSlov;
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Button buttonUlozit;
+        private System.Windows.Forms.TextBox textBoxUkol1;
+
+        #endregion
+    }
+}
---
+++ Buchtík_2023-04-24_test/Buchtík_2023-04-24_test/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Buchtík_2023_04_24_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void buttonUlozit_Click(object sender, EventArgs e)
+        {
+            string veta = textBoxUkol1.Text;
+            using (StreamWriter sWriter = new StreamWriter("vety.txt", true))
+            {
+                if (File.Exists("vety.txt"))
+                {
+                    sWriter.WriteLine(veta);
+                    sWriter.Flush();
+                }
+            }
+        }
+
+        string vetaUkol2;
+        int pocetSlov;
+
+        private void buttonPocetSlov_Click(object sender, EventArgs e)
+        {
+            vetaUkol2 = textBoxUkol2.Text;
+
+            for (int i = 0; i < vetaUkol2.Length; i++)
+            {
+                if (vetaUkol2[i] == ' ')
+                {
+                    pocetSlov++;
+                }
+            }
+
+            textBoxPocetSlov.Text = Convert.ToString(pocetSlov + 1);
+        }
+
+        private string vetaUkol3;
+
+        private void buttonDruhVety_Click(object sender, EventArgs e)
+        {
+            vetaUkol3 = textBoxUkol3.Text;
+            for (int i = 0; i < vetaUkol3.Length; i++)
+            {
+                int delkaVety = vetaUkol3.Length;
+                if (vetaUkol3[delkaVety - 1] == '.')
+                {
+                    textBoxDruhVety.Text = "Věta oznamovací.";
+                }
+                else if (vetaUkol3[delkaVety - 1] == '?')
+                {
+                    textBoxDruhVety.Text = "Věta tázací.";
+                }
+                else if (vetaUkol3[delkaVety - 1] == '!')
+                {
+                    textBoxDruhVety.Text = "Věta rozkazovací.";
+                }
+                else
+                {
+                    textBoxDruhVety.Text = "Žádná z vět.";
+                }
+            }
+        }
+
+        private string vetaUkol4;
+
+        private void buttonPrvniSlovo_Click(object sender, EventArgs e)
+        {
+            textBoxPrvniSlovo.Text = "";
+            vetaUkol4 = textBoxUkol4.Text;
+            for (int i = 0; i < vetaUkol4.Length; i++)
+            {
+                if (vetaUkol4[i] != ' ')
+                {
+                    textBoxPrvniSlovo.Text += vetaUkol4[i];
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
