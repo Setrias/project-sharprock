972e16ba04f9ae2131488695fe704e061adfa13b
Setrias <tomas.buchtik@borsice.net>
2023-05-20 19:12:25 +0200

    Sluneční soustava 2023-05-20 začátek

---
+++ 2023-05-15 ukoly/slunecni_soustava/slunecni_soustava/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace slunecni_soustava
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxPlaneta = new System.Windows.Forms.TextBox();
+            this.textBoxObeznaDoba = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxVzdalenostAu = new System.Windows.Forms.TextBox();
+            this.textBoxVzdalenostKm = new System.Windows.Forms.TextBox();
+            this.textBoxPrumernaRychlost = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxPlaneta
+            // 
+            this.textBoxPlaneta.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPlaneta.Location = new System.Drawing.Point(99, 68);
+            this.textBoxPlaneta.Name = "textBoxPlaneta";
+            this.textBoxPlaneta.Size = new System.Drawing.Size(117, 23);
+            this.textBoxPlaneta.TabIndex = 0;
+            // 
+            // textBoxObeznaDoba
+            // 
+            this.textBoxObeznaDoba.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxObeznaDoba.Location = new System.Drawing.Point(99, 97);
+            this.textBoxObeznaDoba.Name = "textBoxObeznaDoba";
+            this.textBoxObeznaDoba.Size = new System.Drawing.Size(117, 23);
+            this.textBoxObeznaDoba.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Název planety:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Oběžná doba:";
+            // 
+            // textBoxVzdalenostAu
+            // 
+            this.textBoxVzdalenostAu.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxVzdalenostAu.Location = new System.Drawing.Point(157, 175);
+            this.textBoxVzdalenostAu.Name = "textBoxVzdalenostAu";
+            this.textBoxVzdalenostAu.ReadOnly = true;
+            this.textBoxVzdalenostAu.Size = new System.Drawing.Size(117, 23);
+            this.textBoxVzdalenostAu.TabIndex = 4;
+            // 
+            // textBoxVzdalenostKm
+            // 
+            this.textBoxVzdalenostKm.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxVzdalenostKm.Location = new System.Drawing.Point(157, 204);
+            this.textBoxVzdalenostKm.Name = "textBoxVzdalenostKm";
+            this.textBoxVzdalenostKm.ReadOnly = true;
+            this.textBoxVzdalenostKm.Size = new System.Drawing.Size(117, 23);
+            this.textBoxVzdalenostKm.TabIndex = 5;
+            // 
+            // textBoxPrumernaRychlost
+            // 
+            this.textBoxPrumernaRychlost.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPrumernaRychlost.Location = new System.Drawing.Point(157, 233);
+            this.textBoxPrumernaRychlost.Name = "textBoxPrumernaRychlost";
+            this.textBoxPrumernaRychlost.ReadOnly = true;
+            this.textBoxPrumernaRychlost.Size = new System.Drawing.Size(117, 23);
+            this.textBoxPrumernaRychlost.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(12, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Vzdálenost tělesa v AU:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(12, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Vzdálenost tělesa v Km:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(12, 236);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(139, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Průměrná rychlost tělesa:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(262, 38);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Vypočítat";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxPrumernaRychlost);
+            this.Controls.Add(this.textBoxVzdalenostKm);
+            this.Controls.Add(this.textBoxVzdalenostAu);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxObeznaDoba);
+            this.Controls.Add(this.textBoxPlaneta);
+            this.Name = "Form1";
+            this.Text = "Sluneční soustava";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox textBoxVzdalenostKm;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.TextBox textBoxVzdalenostAu;
+        private System.Windows.Forms.TextBox textBoxPrumernaRychlost;
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.TextBox textBoxPlaneta;
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.TextBox textBoxObeznaDoba;
+
+        #endregion
+    }
+}
